4d8b938525bd2c76794a213f52a120fa50d8a227
Mohsen <mabbasidesign@yahoo.com>
2021-04-09 15:01:11 +0430

    Repositories Service

---
+++ Git/Services/RepositoriesService.cs
@@ -0,0 +1,56 @@
+﻿using Git.Data;
+using Git.Models;
+using Git.ViewModel.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git.Services
+{
+    public class RepositoriesService: IRepositoriesService
+    {
+        public readonly ApplicationDbContext db;
+
+        public RepositoriesService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost]
+        public string CreateRepo(string name, string repositoryType, string userId)
+        {
+            var ownerName = this.db.Users
+                .Where(x => x.Id == userId).Select(u => u.Username).FirstOrDefault();
+
+            var owner = this.db.Users.Where(x => x.Id == userId).FirstOrDefault();
+
+            var repo = new Repository
+            {
+                Name = name,
+                CreatedOn = DateTime.UtcNow,
+                OwnerId = owner.Id,
+                Commits = new HashSet<Commit>()
+            };
+
+            this.db.Repositories.Add(repo);
+            this.db.SaveChanges();
+            return owner.Id;
+        }
+
+        public IEnumerable<RepositoryViewModel> GetAll()
+        {
+            var repositories = this.db.Repositories.Select(x => new RepositoryViewModel
+            {
+                Id = x.Id,
+                Commits = x.Commits.Count(),
+                CreatedOn = DateTime.UtcNow,
+                Name = x.Name,
+                Owner = x.Owner.Username
+            }).ToArray();
+
+            return repositories;
+        }
+    }
+}
